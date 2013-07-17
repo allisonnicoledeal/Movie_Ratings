aa8f691b5dd08effccf330c1355f8e9c504fa59c
Hackbright student <user@mini-005.(none)>
2013-07-16 17:56:45 -0700

    Forgot model in previous commit.

--- model.py
+++ model.py
@@ -1,13 +1,14 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, create_engine, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
 
-from sqlalchemy.orm import sessionmaker
-
-ENGINE = None
-Session = None
+engine = create_engine("sqlite:///ratings.db", echo=False)
+session = scoped_session(sessionmaker(bind=engine,
+                                      autocommit=False,
+                                      autoflush = False))
 
 Base = declarative_base()
+Base.query = session.query_property()
 
 ### Class declarations go here
 class User(Base):
@@ -21,6 +22,19 @@ class User(Base):
     gender = Column(String(1), nullable=True)
     occupation = Column(String(32), nullable=True)
 
+    movie_ratings = relationship("Rating", backref=backref("users"))
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    movie_id = Column(Integer, ForeignKey('movies.movie_id'), nullable=False)
+    movie_rating = Column(Integer, nullable=False)
+
+    rater = relationship("User", backref=backref("ratings", order_by=id))
+
+
 class Movie(Base):
     __tablename__ = "movies"
 
@@ -29,28 +43,15 @@ class Movie(Base):
     imdb_url = Column(String(128), nullable=True)
     release_date = Column(DateTime, nullable=True)
 
-class Rating(Base):
-    __tablename__ = "ratings"
+    # use rating class as association object
+    ratings = relationship("Rating", backref="movies")
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False)
-    movie_id = Column(Integer, nullable=False)
-    movie_rating = Column(Integer, nullable=False)
+
 
 
 ### End class declarations
 
-def connect():
-    global ENGINE
-    global Session
-
-    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
-    Session = sessionmaker(bind=ENGINE)
-
-    return Session()
-
 def main():
-    """In case we need this for something"""
     pass
 
 if __name__ == "__main__":
@@ -61,3 +62,11 @@ if __name__ == "__main__":
 ###u.data = id(primary key), user_id, movie_id, movie_rating
 ###u.item = movie_id (primary key), movie_title, IMDB_url, release_date
 ###u.user = user_id(primary key), email_address, password, zip_code, gender=nullable age=nullable
+
+# SQL ALCHEMY NOTES
+# ========================
+#  al = session.query(Movie).filter(Movie.movie_title.like('%Aladdin%')).all()
+#  >>> for user in session.query(Movie).\
+# ...       filter(Movie.release_date>=date1).\
+# ...       filter(Movie.release_date<=date2):
+# ...     print user.release_date
